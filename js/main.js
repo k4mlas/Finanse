6dd8907b24bfcb592a2d94ae4e977cca5c87b1cf
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-26 20:07:23 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@ const expensesSection = document.querySelector(
 	'.transactions__list__expensesArea'
 );
 const wallet = document.querySelector('.options__data__available');
+const deleteSingleExpenses = document.querySelector(
+	'.transactions__list__expensesArea__transaction__delete'
+);
+const deleteSingleIncome = document.querySelector(
+	'.transactions__list__incomeArea__transaction__delete'
+);
 //panel
 const panel = document.querySelector('.panel');
 const inputName = document.querySelector('#name');
 const inputAmount = document.querySelector('#amount');
-const inputSelect = document.querySelector('#select');
+const categorySelect = document.querySelector('#category');
 const save = document.querySelector('.panel__buttons__save');
 const cancel = document.querySelector('.panel__buttons__cancel');
 //options
